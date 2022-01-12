32cab07ed0ebb7528856cc46418042c952bf0759
Paul Ishenin <paul.ishenin@gmail.com>
2022-01-12 21:19:27 +0700

    + экспорт переводов

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './components';
 export * from './context/ChatContext';
 export * from './context/RestContext';
 export * from './context/SocketContext';
+export * from './translations';
---
+++ src/translations/index.ts
@@ -0,0 +1,9 @@
+import en from "./en.json";
+import fr from "./fr.json";
+import ru from "./ru.json";
+
+export const getChatResources = () => ({
+    ru,
+    en,
+    fr
+})
