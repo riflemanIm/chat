12bdb1a31373afb3af082bb0497719467039a637
Oleg Lambin <lambin@postmodern.ru>
2022-03-14 10:46:50 +0300

    ff chatRoomComparer

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -111,13 +111,16 @@ export const getChatName = (chat: ChatRoom): string => {
 };
 
 export const chatRoomComparer = (a: ChatRoom, b: ChatRoom): number => {
-  const hasMessagesA = Array.isArray(a.messages) && (a.messages.length > 0);
-  const hasMessagesB = Array.isArray(b.messages) && (b.messages.length > 0);
-  if (hasMessagesA && hasMessagesB) {
+  const hasMessagesA = Array.isArray(a.messages) && a.messages.length > 0;
+  const hasMessagesB = Array.isArray(b.messages) && b.messages.length > 0;
+  if (
+    hasMessagesA &&
+    hasMessagesB &&
+    b.messages != null &&
+    a.messages != null
+  ) {
     return (
-      // @ts-ignore
       new Date(b.messages[b.messages.length - 1].cdate).getTime() -
-      // @ts-ignore
       new Date(a.messages[a.messages.length - 1].cdate).getTime()
     );
   }
