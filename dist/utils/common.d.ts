ed7a009140655cb9aeded51ceae378c7a13e67bc
Oleg Lambin <lambin@postmodern.ru>
2022-06-03 16:51:51 +0300

    build

--- dist/utils/common.d.ts
+++ dist/utils/common.d.ts
@@ -10,7 +10,7 @@ export declare function isUrl(text: string): boolean;
  * Формитирование времени сообщения
  * @param time
  */
-export declare function formatTime(time: Date | string | undefined): string | Date | null;
+export declare function formatTime(time: Date | string | undefined): string | null;
 /**
  * Раскрыть содержимое
  * @param content - данные в строке
