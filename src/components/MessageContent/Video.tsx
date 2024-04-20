4d90da46d52c553af1dcf1e3109c569bc414bc1d
Oleg La <oleglambin@gmail.com>
2024-04-20 12:49:56 +0300

    fix: Video css style

--- src/components/MessageContent/Video.tsx
+++ src/components/MessageContent/Video.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/material/styles";
-import { ChatMessage } from "../../types";
-import { combineURLs } from "../../utils/common";
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+import { ChatMessage } from '../../types';
+import { combineURLs } from '../../utils/common';
 
 const useStyles = makeStyles((theme: Theme) => ({
   mediaContent: {
     maxWidth: 284,
     maxHeight: 190,
+    backgroundColor: theme.palette.secondary.main,
     borderRadius: theme.spacing(1.2),
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       maxWidth: 250,
-      maxHeight: 210
-    }
-  }
+      maxHeight: 210,
+    },
+  },
 }));
 
 type VideoProps = {
@@ -25,13 +26,16 @@ type VideoProps = {
 const Video: React.FC<VideoProps> = ({
   apiUrl,
   message,
-  isConference
+  isConference,
 }: VideoProps) => {
   const classes = useStyles();
-  let src = "";
+  let src = '';
   if (isConference) {
     const meta = JSON.parse(message.content);
-    src = combineURLs(apiUrl, `/static/conf/${meta.visitId}/${meta.name}`);
+    src = combineURLs(
+      apiUrl,
+      `/static/conf/${meta.visitId}/${meta.name}`,
+    );
   } else src = combineURLs(apiUrl, `/static/file/${message.content}`);
   return (
     <video
--- src/components/MessageContent/index.tsx
+++ src/components/MessageContent/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import File from "./File";
-import Video from "./Video";
-import Image from "./Image";
-import { ChatMessage } from "../../types";
+import React from 'react';
+import File from './File';
+import Video from './Video';
+import Image from './Image';
+import { ChatMessage } from '../../types';
 
 type MessageProps = {
   apiUrl: string;
@@ -12,21 +12,21 @@ type MessageProps = {
 const MessageContent: React.FC<MessageProps> = ({
   apiUrl,
   message,
-  setViewerData
+  setViewerData,
 }: MessageProps) => {
   switch (message.messageType) {
-    case "text":
+    case 'text':
       return <React.Fragment>{message.content}</React.Fragment>;
-    case "video":
-    case "video_conference":
+    case 'video':
+    case 'video_conference':
       return (
         <Video
           message={message}
           apiUrl={apiUrl}
-          isConference={message.messageType === "video_conference"}
+          isConference={message.messageType === 'video_conference'}
         />
       );
-    case "image":
+    case 'image':
       return (
         <Image
           message={message}
@@ -34,7 +34,7 @@ const MessageContent: React.FC<MessageProps> = ({
           setViewerData={setViewerData}
         />
       );
-    case "file":
+    case 'file':
       return <File message={message} />;
   }
   return null;
